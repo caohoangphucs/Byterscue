53a0dc5f4c0c006b6215e89f574f857448bf59f4
= <=>
2025-03-12 17:27:46 +0700

    Cleaner code

--- Backend/setup.sh
+++ Backend/setup.sh
@@ -4,12 +4,14 @@ sudo apt install ngrok -y
 sudo apt install npm -y
 sudo apt install python3 -y
 sudo apt install python3-pip
-sudo apt install nginx -y
 sudo apt install git
 pip install --upgrade pip --break-system-packages -q
 pip install requests --break-system-packages -q
 pip install flask --break-system-packages -q
 pip install flask_cors --break-system-packages -q
 pip install pathlib --break-system-packages -q
+cd Server/NodeJs
+npm install --yes
+cd ../../
 sudo clear
 echo "Set up is done mother fucker !! "
